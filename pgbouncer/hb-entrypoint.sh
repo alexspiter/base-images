0d76ee14ef6ff27e061443ac9c529a59422822d7
Dao Le <leanhdaovn@gmail.com>
2018-10-03 18:02:49 +0800

    [pgbouncer] add stats and admin user (#13)
    * Add stats user
    * Add admin user

--- pgbouncer/hb-entrypoint.sh
+++ pgbouncer/hb-entrypoint.sh
@@ -7,5 +7,15 @@ AUTH_TYPE="${AUTH_TYPE:-md5}"
 mkdir /etc/pgbouncer
 echo \"$DB_USER\" \"$DB_PASSWORD\" > /etc/pgbouncer/users.txt
 
+# add stats_user
+if [[ ! -z "${STATS_USER}" ]]; then
+  echo \"$STATS_USER\" \"$STATS_USER_PASSWORD\" >> /etc/pgbouncer/users.txt
+fi
+
+# add admin_user
+if [[ ! -z "${ADMIN_USER}" ]]; then
+  echo \"$ADMIN_USER\" \"$ADMIN_USER_PASSWORD\" >> /etc/pgbouncer/users.txt
+fi
+
 # run parent entry point
 ./entrypoint.sh
